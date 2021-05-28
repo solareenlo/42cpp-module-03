29a58e8d65a9e695cab32ab054c882356b0e14b7
solareenlo <solareenlo@gmail.com>
2021-05-28 17:35:07 +0900

    update: ex01/main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 07:06:33 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/05/28 15:48:48 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/05/28 17:33:23 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
@@ -19,26 +19,38 @@ int main(void) {
 
     fragTrap.rangeAttack("B");
     scavTrap.rangeAttack("B");
+    std::cout << std::endl;
+
     fragTrap.meleeAttack("B");
     scavTrap.meleeAttack("B");
+    std::cout << std::endl;
+
     fragTrap.takeDamage(10);
     scavTrap.takeDamage(10);
     fragTrap.takeDamage(10);
     scavTrap.takeDamage(10);
     fragTrap.takeDamage(100);
+    std::cout << std::endl;
+
     fragTrap.meleeAttack("B");
     fragTrap.beRepaired(10);
     fragTrap.beRepaired(100);
     scavTrap.beRepaired(100);
     fragTrap.beRepaired(100);
+    std::cout << std::endl;
+
     fragTrap.vaulthunter_dot_exe("B");
     fragTrap.vaulthunter_dot_exe("C");
     fragTrap.vaulthunter_dot_exe("D");
     fragTrap.vaulthunter_dot_exe("E");
     fragTrap.vaulthunter_dot_exe("F");
     fragTrap.vaulthunter_dot_exe("G");
+    std::cout << std::endl;
+
     scavTrap.challengeNewcomer("A");
     scavTrap.challengeNewcomer("B");
+    std::cout << std::endl;
+
     fragTrap.takeDamage(200);
     scavTrap.takeDamage(200);
     fragTrap.vaulthunter_dot_exe("G");
